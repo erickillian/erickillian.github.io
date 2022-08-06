e9c880ee29be8bdd49a02bc7aaaa8cae91ddbf98
erickillian <erickillian39@gmail.com>
2022-08-06 15:51:11 -0400

    Adds convergle project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,40 +3,50 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import pong from "../../assets/images/projects/converge-pong.png";
 import portfolio_website from "../../assets/images/projects/website.png";
+import convergle from "../../assets/images/projects/wordle.png";
 
 function Projects() {
-  return (
-    <Container fluid className="project-section">
-      <Container>
-        <h1 className="project-heading">
-          My Recent <strong className="important">Works </strong>
-        </h1>
-        <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
-        </p>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={pong}
-              isBlog={false}
-              title="Converge Pong"
-              description="App that allows employees of Converge Technologies (local start up) to compare and record Ping Pong scores. Uses an ELO point scoring system"
-              link="https://github.com/erickillian/converge-ping-pong"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={portfolio_website}
-              isBlog={false}
-              title="Portfolio Website"
-              description="Portfolio Website that uses React.js"
-              link="https://github.com/erickillian/erickillian.github.io"
-            />
-          </Col>
-        </Row>
-      </Container>
-    </Container>
-  );
+    return (
+        <Container fluid className="project-section">
+            <Container>
+                <h1 className="project-heading">
+                    My Recent <strong className="important">Works </strong>
+                </h1>
+                <p style={{ color: "white" }}>
+                    Here are a few projects I've worked on recently.
+                </p>
+                <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            imgPath={convergle}
+                            isBlog={false}
+                            title="Convergle"
+                            description="Fun competitive app that allows members of Converge Technologies (local start up) to play and compete at wordle"
+                            link="https://github.com/erickillian/wordle-app"
+                        />
+                    </Col>
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            imgPath={pong}
+                            isBlog={false}
+                            title="Converge Pong"
+                            description="App that allows employees of Converge Technologies (local start up) to compare and record Ping Pong scores. Uses an ELO point scoring system"
+                            link="https://github.com/erickillian/converge-ping-pong"
+                        />
+                    </Col>
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            imgPath={portfolio_website}
+                            isBlog={false}
+                            title="Portfolio Website"
+                            description="Portfolio Website that uses React.js"
+                            link="https://github.com/erickillian/erickillian.github.io"
+                        />
+                    </Col>
+                </Row>
+            </Container>
+        </Container>
+    );
 }
 
 export default Projects;
